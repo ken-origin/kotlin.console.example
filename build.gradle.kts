78f4251947ad8e4cd5435c41990a6c6c0268c0ac
ken-origin <k.nishikawa572086@gmail.com>
2021-05-16 16:38:00 +0900

    初期コミット

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.internal.encodePluginOptions
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+
+group = "me.kentarou"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    // Listの初期化
+    val list: List<Int> = listOf(1, 2, 3)
+    val listMt: MutableList<Int> = mutableListOf(1, 2, 3)
+
+    // Setの初期化
+    val set: Set<Int> = setOf(1, 2, 3)
+    val setMt: MutableSet<Int> = mutableSetOf(1, 2, 3)
+
+    // Mapの初期化
+    val map: Map<String, Int> = mapOf("one" to 1, "two" to 2)
+    val mapMt: MutableMap<String, Int> = mutableMapOf("one" to 1, "two" to 2)
+
+    // List -> Map変換
+    val listToMap = list.associateBy({it}, {"test${it}"})
+    // Map -> Map変換
+    val mapToMap = map.map { it.key to it.value.toString() }.toMap()
+
+    // 表示１
+    listToMap.forEach { (t, u) -> println("listからMap変換 t:${t} u:${u}") }
+    // 表示２
+    mapToMap.forEach { (t, u) -> println("MapからMap変換 t:${t} u:${u}") }
+
+    println("Hello World!")
+}
