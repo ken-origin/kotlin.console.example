a2cf36f893761c816b864edfe819a0266222cf58
ken-origin <k.nishikawa572086@gmail.com>
2021-05-25 23:05:35 +0900

    整形

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,24 @@
 
 fun main(args: Array<String>) {
+    println(args)
     // Listの初期化
     val list: List<Int> = listOf(1, 2, 3)
     val listMt: MutableList<Int> = mutableListOf(1, 2, 3)
+
+    println(listMt)
 
     // Setの初期化
     val set: Set<Int> = setOf(1, 2, 3)
     val setMt: MutableSet<Int> = mutableSetOf(1, 2, 3)
 
+    println(set)
+    println(setMt)
+
     // Mapの初期化
     val map: Map<String, Int> = mapOf("one" to 1, "two" to 2)
     val mapMt: MutableMap<String, Int> = mutableMapOf("one" to 1, "two" to 2)
+
+    println(mapMt)
 
     // List -> Map変換
     val listToMap = list.associateBy({it}, {"test${it}"})
@@ -22,7 +30,8 @@ fun main(args: Array<String>) {
     // 表示２
     mapToMap.forEach { (t, u) -> println("MapからMap変換 t:${t} u:${u}") }
 
-    val ex: Throwable = if (args == null) {
+    val tests = Math.random().toInt()
+    val ex: Throwable = if (tests > 0) {
         NullPointerException()
     } else {
         NumberFormatException()
